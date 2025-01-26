350a56b1f85940345ef0eee296a0204e67be987b
budi priyanto <mas.bdp@gmail.com>
2025-01-26 18:02:51 +0700

    first commit

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -1,7 +1,5 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 defined('BASEPATH') OR exit('No direct script access allowed');
-
-
 require APPPATH . '/libraries/REST_Controller.php';
 use Restserver\Libraries\REST_Controller;
  
